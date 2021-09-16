6962aa64e038ec8a111a317e69dd04a975d06e2e
Luna Sarmiento <84574252+lunisel@users.noreply.github.com>
2021-09-16 17:46:28 +0200

    Workingggg

--- src/reducers/jobs.js
+++ src/reducers/jobs.js
@@ -5,34 +5,22 @@ const jobsReducer = (state = initialState.jobs, action) => {
     case "FILL_JOBS":
       return {
         ...state,
-        jobs: {
-          ...state.jobs,
-          allJobs: action.payload
-        }
+        allJobs: action.payload
       };
     case "ADD_SELECTED_JOB":
       return {
         ...state,
-        jobs: {
-          ...state.jobs,
-          selectedJob: action.payload
-        }
+        selectedJob: action.payload
       };
     case "FILL_JOBS_LOADING":
       return {
         ...state,
-        jobs: {
-          ...state.jobs,
-          isLoading: action.payload
-        }
+        isLoading: action.payload
       };
     case "FILL_JOBS_ERROR":
       return {
         ...state,
-        jobs: {
-          ...state.jobs,
-          error: action.payload
-        }
+        error: action.payload
       };
     default:
       return state;
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -5,28 +5,17 @@ const userReducer = (state = initialState.user, action) => {
     case "SET_USER":
       return {
         ...state,
-        user: {
-          ...state.user,
-          email: action.payload
-        }
+        email: action.payload
       };
     case "ADD_FAVOURITE":
       return {
         ...state,
-        user: {
-          ...state.user,
-          favourite: [...state.user.favourite, action.payload]
-        }
+        favourite: [...state.favourite, action.payload]
       };
     case "REMOVE_FAVOURITE":
       return {
         ...state,
-        user: {
-          ...state.user,
-          favourite: state.user.favourite.filter(
-            (job, i) => i !== action.payload
-          )
-        }
+        favourite: state.favourite.filter((job, i) => i !== action.payload)
       };
     default:
       return state;
