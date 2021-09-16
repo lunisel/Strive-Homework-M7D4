ceb6486aff4f5d9ed06dcbb09288c70d7c8cb57f
Luna Sarmiento <84574252+lunisel@users.noreply.github.com>
2021-09-16 16:50:09 +0200

    Actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,24 +1,75 @@
 export const setUserAction = (email) => ({
   type: "SET_USER",
-  payload: email,
-});
-
-export const setQueryAction = (queryToAdd) => ({
-  type: "ADD_QUERY",
-  payload: queryToAdd,
+  payload: email
 });
 
 export const addToFavouritesAction = (job) => ({
   type: "ADD_FAVOURITE",
-  payload: job,
+  payload: job
 });
 
 export const removeFromFavouritesAction = (job) => ({
   type: "REMOVE_FAVOURITE",
-  payload: job,
+  payload: job
 });
 
 export const addSelectedJobAction = (job) => ({
   type: "ADD_SELECTED_JOB",
-  payload: job,
+  payload: job
 });
+
+export const fillJobsAction = (searchString) => {
+  return async (dispatch, getState) => {
+    const baseUrl = "https://strive-jobs-api.herokuapp.com/jobs?";
+    try {
+      let resp = await fetch(
+        searchString ? baseUrl + "title=" + searchString : baseUrl + "limit=18"
+      );
+      if (resp.ok) {
+        let data = await resp.json();
+        let jobs = data.data;
+        setTimeout(() => {
+          dispatch({
+            type: "FILL_JOBS_LOADING",
+            payload: false
+          });
+        }, 1000);
+        dispatch({
+          type: "FILL_JOBS_ERROR",
+          payload: false
+        });
+        dispatch({
+          type: "FILL_JOBS",
+          payload: jobs
+        });
+      } else {
+        setTimeout(() => {
+          dispatch({
+            type: "FILL_JOBS_LOADING",
+            payload: false
+          });
+        }, 1000);
+        setTimeout(() => {
+          dispatch({
+            type: "FILL_JOBS_ERROR",
+            payload: true
+          });
+        }, 1000);
+      }
+    } catch (err) {
+      console.log(err);
+      setTimeout(() => {
+        dispatch({
+          type: "FILL_JOBS_LOADING",
+          payload: false
+        });
+      }, 1000);
+      setTimeout(() => {
+        dispatch({
+          type: "FILL_JOBS_ERROR",
+          payload: true
+        });
+      }, 1000);
+    }
+  };
+};
