140ca80990d923632ce8af4131b3d52236b14ae5
Luna Sarmiento <selene942008@hotmail.com>
2021-09-20 16:11:35 +0200

    persist

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import configureStore from './store';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { persistor, configureStore } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={configureStore}>
-    <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import userReducer from "../reducers/user.js";
 import jobsReducer from "../reducers/jobs.js";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
@@ -19,17 +21,29 @@ export const initialState = {
   },
 };
 
+const persistConfig = {
+  key: "root",
+  storage,
+  /* transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_ENCRYPT_KEY,
+    }),
+  ], */
+};
+
 const bigReducers = combineReducers({
   jobs: jobsReducer,
   user: userReducer,
 });
 
-const configureStore = createStore(
-  bigReducers,
+const persistedReducer = persistReducer(persistConfig, bigReducers);
+
+export const configureStore = createStore(
+  persistedReducer,
   initialState,
   process.env.REACT_APP_DEVELOPMENT
     ? composeEnhancers(applyMiddleware(thunk))
     : compose(applyMiddleware(thunk))
 );
 
-export default configureStore;
+export const persistor = persistStore(configureStore);
